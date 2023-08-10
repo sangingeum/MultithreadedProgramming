1b90f5c003a38fa2a4329e8273b14a79f05a0fa9
Sangin Geum <56298463+sangingeum@users.noreply.github.com>
2023-08-10 20:14:52 +0900

    Edit example code
    Edit example code

--- ThreadPool/ThreadPool.hpp
+++ ThreadPool/ThreadPool.hpp
@@ -35,7 +35,8 @@ public:
 
 	template <class Func>
 	std::future<typename std::invoke_result<Func>::type> submit(Func func) {
-		std::packaged_task task{std::move(func)};
+		using ResultType = typename std::invoke_result<Func>::type;
+		std::packaged_task<ResultType()> task{std::move(func)};
 		auto future{task.get_future()};
 		// Wrap the packaged task with a lambda function to store it in the queue
 		m_queue.push([t = std::make_shared<decltype(task)>(std::move(task))]() { (*t)(); });
--- ThreadPool/main.cpp
+++ ThreadPool/main.cpp
@@ -1,20 +1,101 @@
 #include "ThreadPool.hpp"
 #include <vector>
 #include <iostream>
+#include <string>
 
-void hi() {
+void sayHi() {
 	std::cout << "Hi\n";
 }
 
+void saySomething(const std::string& something) {
+	std::cout << something + "\n";
+}
+
+std::vector<int> accumulate(const std::vector<int>& vec) {
+	auto result = vec;
+	for (size_t i = 1; i < result.size(); ++i)
+		result[i] = std::move(result[i]) + result[i - 1];
+	return result;
+}
+
+int five() {
+	return 5;
+}
+
 int main() {
+	// Create a thread pool
 	ThreadPool pool;
-	std::vector<std::future<void>> futures;
-	for (size_t i = 0; i < 100; ++i) {
-		futures.emplace_back(pool.submit(hi));
+	// Containers for futures
+	std::vector<std::future<void>> voidFutures;
+	std::vector<std::future<std::vector<int>>> vecFutures;
+	std::vector<std::future<int>> intFutures;
+
+	// Submit tasks to the pool
+	// std::function, std::bind, lambda, and function name can be passed 
+	std::vector<int> vec {1, 3, 5, 7};
+	for (size_t i = 0; i < 10; ++i) {
+		voidFutures.emplace_back(pool.submit(sayHi));
+		voidFutures.emplace_back(pool.submit([]() {saySomething("Something"); }));
+		vecFutures.emplace_back(pool.submit(std::bind(accumulate, std::cref(vec))));
+		intFutures.emplace_back(pool.submit(five));
 	}
-	for (auto& future : futures) {
+
+	// Join and print the results
+	for (auto& future : voidFutures) {
 		future.get();
 	}
+	for (auto& future : vecFutures) {
+		auto vec = future.get();
+		for (auto num : vec)
+			std::cout << num << " ";
+		std::cout << "\n";
+	}
+	for (auto& future : intFutures) {
+		std::cout << future.get() << "\n";
+	}
+
+	/* Possible result:
+	Hi
+	Something
+	Something
+	Hi
+	Something
+	Something
+	Something
+	Something
+	Hi
+	Hi
+	Hi
+	Something
+	Hi
+	Something
+	Hi
+	Something
+	Hi
+	Something
+	Hi
+	Hi
+	1 4 9 16
+	1 4 9 16
+	1 4 9 16
+	1 4 9 16
+	1 4 9 16
+	1 4 9 16
+	1 4 9 16
+	1 4 9 16
+	1 4 9 16
+	1 4 9 16
+	5
+	5
+	5
+	5
+	5
+	5
+	5
+	5
+	5
+	5
+	*/
 
 	return 0;
 }
